e857acfe5e2be39fe4f910ced41c749f72935ba3
ignalauret <ignalau@gmail.com>
2020-08-08 12:18:46 -0300

    Add and delete users

--- js/add_client.js
+++ js/add_client.js
@@ -1,32 +1,3 @@
-const addClient = (token, name, dni) => {
-  // Build auth sufix with token
-  var authSufix = "auth=" + token;
-  var firstName = name.split(" ")[0];
-  console.log(dni);
-  console.log(firstName);
-  $.ajax({
-    url: "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDMsEID7PGSNpM5EySROO3iA-eUhcO_KPo",
-    type: "POST",
-    headers: {'Content-Type': 'application/json',},
-    data: '{"email":"' + dni + "@iltempo.com" + '","password":"' + firstName + '","returnSecureToken": true}',
-    success: function(data) {
-      if(data != null) {
-        var uid = data["localId"];
-        var patchData = '{"dni":"' + dni + '","nombre":"' + name + '"}';
-        // Musculación
-        $.ajax({
-          url: 'https://il-tempo-dda8e.firebaseio.com/usuarios/' + uid + '.json?'+ authSufix,
-          type: "PATCH",
-          data: patchData,
-        });
-      }
-    },
-    error: function(xhr, status, error) {
-      console.log(eval("(" + xhr.responseText + ")"));
-    }
-  });
-}
-
 const setCookie = (cname, cvalue, exseconds) => {
   var d = new Date();
   d.setTime(d.getTime() + (exseconds*1000));
@@ -50,10 +21,126 @@ const getCookie = (cname) => {
   return "";
 }
 
+const addClient = (token, name, dni, password) => {
+  // Build auth sufix with token
+  var authSufix = "auth=" + token;
+  $.ajax({
+    url: "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDMsEID7PGSNpM5EySROO3iA-eUhcO_KPo",
+    type: "POST",
+    headers: {'Content-Type': 'application/json',},
+    data: '{"email":"' + dni + "@iltempo.com" + '","password":"' + password + '","returnSecureToken": false}',
+    success: function(data) {
+      if(data != null) {
+        var uid = data["localId"];
+        var patchData = '{"dni":"' + dni + '","nombre":"' + name + '","psw":"' + password + '"}';
+        // Musculación
+        $.ajax({
+          url: 'https://il-tempo-dda8e.firebaseio.com/usuarios/' + uid + '.json?'+ authSufix,
+          type: "PATCH",
+          data: patchData,
+          success: function(data) {
+            alert("El usuario " + name + " ha sido agregado correctamente.");
+          }
+        });
+      }
+    },
+    error: function(xhr, status, error) {
+      alert("Lo sentimos, hubo un error.")
+    }
+  });
+}
+
+const getClients = (token) => {
+  // Build auth sufix with token
+  var authSufix = "auth=" + token;
+  // Get all turns.
+  $.getJSON('https://il-tempo-dda8e.firebaseio.com/usuarios.json?' + authSufix, function(data) {
+    if(data == null) return;
+    var dataSet = [];
+    dataArray = Object.values(data);
+    idArray = Object.keys(data);
+    for(var i = 0; i < dataArray.length; i++) {
+      dataSet.push([idArray[i], dataArray[i].nombre, dataArray[i].dni]);
+    }
+
+    $('#users_table').DataTable( {
+        data: dataSet,
+        columns: [
+            { title: "ID" },
+            { title: "Nombre" },
+            { title: "Dni" },
+          ]
+    });
+  });
+}
+
+const deleteClient = (token, id) => {
+  // Build auth sufix with token
+  var authSufix = "auth=" + token;
+  // Get user mail and password
+  $.ajax({
+    url: 'https://il-tempo-dda8e.firebaseio.com/usuarios/' + id + '.json?' + authSufix,
+    type: "GET",
+    success: function(userData) {
+      // Login user to get token
+      var loginUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDMsEID7PGSNpM5EySROO3iA-eUhcO_KPo";
+      $.post(loginUrl, {
+                "email": userData["dni"] + "@iltempo.com",
+                "password": userData["psw"],
+                "returnSecureToken": true,
+              },function(authData, status, jqXHR) {
+                // Delete user from Auth
+                idToken = authData["idToken"];
+                $.ajax({
+                  url: "https://identitytoolkit.googleapis.com/v1/accounts:delete?key=AIzaSyDMsEID7PGSNpM5EySROO3iA-eUhcO_KPo",
+                  type: "POST",
+                  headers: {'Content-Type': 'application/json',},
+                  data: '{"idToken":"' + idToken + '"}',
+                  success: function(data) {
+                    // Delete user data from db
+                    $.ajax({
+                      url: 'https://il-tempo-dda8e.firebaseio.com/usuarios/' + id + '.json?' + authSufix,
+                      type: "DELETE",
+                      success: function(data) {
+                        alert("El usuario " + userData["nombre"] + " ha sido eliminado.");
+                      }
+                    });
+                  },
+                  error: function(xhr, status, error) {
+                    console.log(eval("(" + xhr.responseText + ")"));
+                  }
+                });
+              });
+    }
+    error: function(xhr, status, error) {
+      alert("Lo sentimos, hubo un error.")
+    }
+  });
+}
+
+
+$(document).ready(function() {
+  var token = getCookie("token");
+  if(token == "") {
+    var loginUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDMsEID7PGSNpM5EySROO3iA-eUhcO_KPo";
+    var response = $.post(loginUrl, {
+              "email": "ignalau@iltempo.com",
+              "password": "contraseña123",
+              "returnSecureToken": true,
+            },function(data, status, jqXHR) {
+              setCookie("token", data["idToken"], data["expiresIn"]);
+              getClients(data["idToken"]);
+            });
+  } else {
+    getClients(token);
+  }
+});
+
 $('#add_button').click(function() {
   var token = getCookie("token");
   var name = $('#add_name').val();
   var dni = $('#add_dni').val();
+  var password = $('#add_password').val();
   if(token == "") {
     var loginUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDMsEID7PGSNpM5EySROO3iA-eUhcO_KPo";
     var response = $.post(loginUrl, {
@@ -63,9 +150,29 @@ $('#add_button').click(function() {
             },function(data, status, jqXHR) {
               token = data["idToken"];
               setCookie("token", token, data["expiresIn"]);
-              addClient(token, name, dni);
+              addClient(token, name, dni, password);
             });
   } else {
-    addClient(token, id);
+    addClient(token, name, dni, password);
+  }
+});
+
+$('#delete_button').click(function() {
+  var token = getCookie("token");
+  var id = $('#delete_id').val();
+  if (id == "") return;
+  if(token == "") {
+    var loginUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDMsEID7PGSNpM5EySROO3iA-eUhcO_KPo";
+    var response = $.post(loginUrl, {
+              "email": "test@iltempo.com",
+              "password": "contraseña123",
+              "returnSecureToken": true,
+            },function(data, status, jqXHR) {
+              token = data["idToken"];
+              setCookie("token", token, data["expiresIn"]);
+              deleteClient(token, id);
+            });
+  } else {
+    deleteClient(token, id);
   }
 });
