ea1ac63aef0998d196a83a1456ff7b6f41367b48
ignalauret <ignalau@gmail.com>
2020-11-02 09:55:57 -0300

    Ritmos

--- js/training.js
+++ js/training.js
@@ -1,8 +1,9 @@
 const updateTraining = (token) => {
   var currentTraining = getCurrentTraining();
   var dbName = currentTraining.charAt(0).toUpperCase() + currentTraining.slice(1);
-  if(dbName == "Funcional") dbName = "Funcional Grupal";
-  if(dbName == "Musculacion") dbName = "Musculación";
+  if (dbName == "Funcional") dbName = "Funcional Grupal";
+  if (dbName == "Musculacion") dbName = "Musculación";
+  if (dbName == "Zumba") dbName = "Ritmos";
   authSufix = "auth=" + token;
   schedule = ',"horario":{';
   var arr_days = [
@@ -12,43 +13,43 @@ const updateTraining = (token) => {
     $('#' + currentTraining + '_jueves').val().split(","),
     $('#' + currentTraining + '_viernes').val().split(","),
   ];
-  if(currentTraining == "musculacion") {
-    arr_days.push($('#musculacion_sabado').val().split(","),);
+  if (currentTraining == "musculacion") {
+    arr_days.push($('#musculacion_sabado').val().split(","));
   }
   var count = 0;
-  for(var i = 0; i < arr_days.length; i++) {
-    for(var horario of arr_days[i]) {
-      if(horario == "") continue;
-      if(horario.includes("a")) {
+  for (var i = 0; i < arr_days.length; i++) {
+    for (var horario of arr_days[i]) {
+      if (horario == "") continue;
+      if (horario.includes("a")) {
         var horarios = horario.split(" a ");
         var first = horarios[0].split(":");
         var second = horarios[1].split(":");
-        schedule += '"' + count + '":"4.' + (20+i) + '.' + first[0] + '.' + first[1] + 'a' + '4.'+ (20+i) + '.' + second[0] + '.' + second[1] + '",';
+        schedule += '"' + count + '":"4.' + (20 + i) + '.' + first[0] + '.' + first[1] + 'a' + '4.' + (20 + i) + '.' + second[0] + '.' + second[1] + '",';
         count++;
       } else {
         var arr = horario.split(":");
-        schedule += '"' +count + '":"4.'  + (20+i) + '.' + arr[0] + '.' + arr[1] + '",';
+        schedule += '"' + count + '":"4.' + (20 + i) + '.' + arr[0] + '.' + arr[1] + '",';
         count++;
       }
     }
   }
-  schedule = schedule.substr(0,schedule.length-1);
+  schedule = schedule.substr(0, schedule.length - 1);
   schedule += '}';
   var patchData = '{"intervalo":"' + $('#' + currentTraining + '_duration_mins').val() + '","profesor":"' + $('#' + currentTraining + '_teacher').val() + '","duration":"' + $('#' + currentTraining + '_duration').val() + '","descripcion":"' + $('#' + currentTraining + '_description').val() + '","maxSchedules":' + $('#' + currentTraining + '_max').val() + schedule;
-  if(currentTraining == "musculacion") {
+  if (currentTraining == "musculacion") {
     patchData += ',"freeGymMaxSchedules":' + $('#musculacion_free_max').val() + "}";
   } else {
     patchData += "}";
   }
   // Musculación
   $.ajax({
-    url: 'https://il-tempo-dda8e.firebaseio.com/trainings/' + dbName + '.json?'+ authSufix,
+    url: 'https://il-tempo-dda8e.firebaseio.com/trainings/' + dbName + '.json?' + authSufix,
     type: "PATCH",
     data: patchData,
-    success: function(data) {
+    success: function (data) {
       alert("La información de la clase ha sido actualizada correctamente");
     },
-    error: function(data) {
+    error: function (data) {
       alert("Lo sentimos, hubo un error al actualizar la información");
     }
   });
@@ -58,18 +59,19 @@ const updateTraining = (token) => {
 const getTraining = (token) => {
   var currentTraining = getCurrentTraining();
   var dbName = currentTraining.charAt(0).toUpperCase() + currentTraining.slice(1);
-  if(dbName == "Funcional") dbName = "Funcional Grupal";
-  if(dbName == "Musculacion") dbName = "Musculación";
+  if (dbName == "Funcional") dbName = "Funcional Grupal";
+  if (dbName == "Musculacion") dbName = "Musculación";
+  if (dbName == "Zumba") dbName = "Ritmos";
   var authSufix = "auth=" + token;
-  const url = 'https://il-tempo-dda8e.firebaseio.com/trainings/' + dbName + '.json?'+ authSufix;
-  $.getJSON(url, function(data) {
+  const url = 'https://il-tempo-dda8e.firebaseio.com/trainings/' + dbName + '.json?' + authSufix;
+  $.getJSON(url, function (data) {
     $('#' + currentTraining + '_teacher').val(data["profesor"]);
     $('#' + currentTraining + '_description').val(data["descripcion"]);
     $('#' + currentTraining + '_max').val(data["maxSchedules"]);
     $('#' + currentTraining + '_duration').val(data["duration"]);
     $('#' + currentTraining + '_duration_mins').val(data["intervalo"]);
 
-    if(currentTraining == "musculacion") {
+    if (currentTraining == "musculacion") {
       $('#musculacion_free_max').val(data["freeGymMaxSchedules"]);
     }
 
@@ -79,8 +81,8 @@ const getTraining = (token) => {
     var horarios_jueves = "";
     var horarios_viernes = "";
     var horarios_sabados = "";
-    for(var horario of data["horario"]) {
-      if(horario.includes("a")) {
+    for (var horario of data["horario"]) {
+      if (horario.includes("a")) {
         var horarios = horario.split("a");
         var first = horarios[0].split(".");
         var second = horarios[1].split(".");
@@ -107,7 +109,7 @@ const getTraining = (token) => {
         }
       } else {
         var arr = horario.split(".");
-        if(arr[3].length == 1) arr[3] += "0";
+        if (arr[3].length == 1) arr[3] += "0";
         switch (arr[1]) {
           case "20":
             horarios_lunes += (arr[2] + ":" + arr[3] + ",");
@@ -136,43 +138,43 @@ const getTraining = (token) => {
     $('#' + currentTraining + '_miercoles').val(horarios_miercoles);
     $('#' + currentTraining + '_jueves').val(horarios_jueves);
     $('#' + currentTraining + '_viernes').val(horarios_viernes);
-    if(currentTraining == "musculacion") {
+    if (currentTraining == "musculacion") {
       $('#musculacion_sabado').val(horarios_sabados);
     }
-});
+  });
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
   var token = getCookie("token");
-  if(token == "") {
+  if (token == "") {
     var loginUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDMsEID7PGSNpM5EySROO3iA-eUhcO_KPo";
     var response = $.post(loginUrl, {
-              "email": "test@iltempo.com",
-              "password": "contraseña123",
-              "returnSecureToken": true,
-            },function(data, status, jqXHR) {
-              token = data["idToken"];
-              setCookie("token", token, data["expiresIn"]);
-              getTraining(token);
-            });
+      "email": "test@iltempo.com",
+      "password": "contraseña123",
+      "returnSecureToken": true,
+    }, function (data, status, jqXHR) {
+      token = data["idToken"];
+      setCookie("token", token, data["expiresIn"]);
+      getTraining(token);
+    });
   } else {
     getTraining(token);
   }
 });
 
-$('#' + getCurrentTraining() + '_button').click(function() {
+$('#' + getCurrentTraining() + '_button').click(function () {
   var token = getCookie("token");
-  if(token == "") {
+  if (token == "") {
     var loginUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDMsEID7PGSNpM5EySROO3iA-eUhcO_KPo";
     var response = $.post(loginUrl, {
-              "email": "test@iltempo.com",
-              "password": "contraseña123",
-              "returnSecureToken": true,
-            },function(data, status, jqXHR) {
-              token = data["idToken"];
-              setCookie("token", token, data["expiresIn"]);
-              updateTraining(token);
-            });
+      "email": "test@iltempo.com",
+      "password": "contraseña123",
+      "returnSecureToken": true,
+    }, function (data, status, jqXHR) {
+      token = data["idToken"];
+      setCookie("token", token, data["expiresIn"]);
+      updateTraining(token);
+    });
   } else {
     updateTraining(token);
   }
--- js/turns.js
+++ js/turns.js
@@ -2,7 +2,7 @@ const getCurrentTraining = () => {
   var currentPath = window.location.pathname;
   var splittedPath = currentPath.split("/");
   var currentTraining = splittedPath[splittedPath.length - 1].split(".")[0];
-  if(currentTraining == "index") {
+  if (currentTraining == "index") {
     currentTraining = "all";
   }
   return currentTraining;
@@ -10,8 +10,8 @@ const getCurrentTraining = () => {
 
 const setCookie = (cname, cvalue, exseconds) => {
   var d = new Date();
-  d.setTime(d.getTime() + (exseconds*1000));
-  var expires = "expires="+ d.toUTCString();
+  d.setTime(d.getTime() + (exseconds * 1000));
+  var expires = "expires=" + d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
@@ -19,14 +19,14 @@ const getCookie = (cname) => {
   var name = cname + "=";
   var decodedCookie = decodeURIComponent(document.cookie);
   var ca = decodedCookie.split(';');
-  for(var i = 0; i <ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-          c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-          return c.substring(name.length, c.length);
-      }
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
   }
   return "";
 }
@@ -37,8 +37,9 @@ const getTurns = (token) => {
   // Get current training
   var currentTraining = getCurrentTraining();
   var dbName = currentTraining.charAt(0).toUpperCase() + currentTraining.slice(1);
-  if(dbName == "Funcional") dbName = "Funcional Grupal";
-  if(dbName == "Musculacion") dbName = "Musculación";
+  if (dbName == "Funcional") dbName = "Funcional Grupal";
+  if (dbName == "Musculacion") dbName = "Musculación";
+  if (dbName == "Zumba") dbName = "Ritmos";
   // Get dates for query
   const date_today = new Date();
   var day_today = date_today.getDate();
@@ -52,35 +53,35 @@ const getTurns = (token) => {
   var tomorrow = day_tomorrow.toString() + "/" + month_tomorrow.toString();
 
   // Get todays turns.
-  $.getJSON('https://il-tempo-dda8e.firebaseio.com/turnos.json?orderBy="fecha"&equalTo="' + today + '"&' + authSufix, function(data) {
-    if(data == null) return;
+  $.getJSON('https://il-tempo-dda8e.firebaseio.com/turnos.json?orderBy="fecha"&equalTo="' + today + '"&' + authSufix, function (data) {
+    if (data == null) return;
     var dataSet = [];
     dataArray = Object.values(data);
     idArray = Object.keys(data);
-    for(var i = 0; i < dataArray.length; i++) {
-      if(dataArray[i].clase == dbName || dbName == "All"){
+    for (var i = 0; i < dataArray.length; i++) {
+      if (dataArray[i].clase == dbName || dbName == "All") {
         dataSet.push([idArray[i], dataArray[i].nombre, dataArray[i].dni, dataArray[i].clase, dataArray[i].hora, dataArray[i].fecha]);
       }
     }
-    $('#' + currentTraining + '_today').DataTable( {
-        data: dataSet,
-        columns: [
-            { title: "ID" },
-            { title: "Nombre" },
-            { title: "Dni" },
-            { title: "Clase" },
-            { title: "Horario" },
-            { title: "Fecha" }        ]
-    } );
+    $('#' + currentTraining + '_today').DataTable({
+      data: dataSet,
+      columns: [
+        { title: "ID" },
+        { title: "Nombre" },
+        { title: "Dni" },
+        { title: "Clase" },
+        { title: "Horario" },
+        { title: "Fecha" }]
+    });
   });
   // Get all turns.
-  $.getJSON('https://il-tempo-dda8e.firebaseio.com/turnos.json?' + authSufix, function(data) {
-    if(data == null) return;
+  $.getJSON('https://il-tempo-dda8e.firebaseio.com/turnos.json?' + authSufix, function (data) {
+    if (data == null) return;
     var dataSet = [];
     dataArray = Object.values(data);
     idArray = Object.keys(data);
-    for(var i = 0; i < dataArray.length; i++) {
-      if(dataArray[i].clase == dbName || dbName == "All"){
+    for (var i = 0; i < dataArray.length; i++) {
+      if (dataArray[i].clase == dbName || dbName == "All") {
         dataSet.push([idArray[i], dataArray[i].nombre, dataArray[i].dni, dataArray[i].clase, dataArray[i].hora, dataArray[i].fecha]);
       }
     }
@@ -89,15 +90,15 @@ const getTurns = (token) => {
     //     dataSet.push([turno.nombre, turno.dni, turno.clase, turno.dia, turno.hora, turno.fecha]);
     //   }
     // }
-    $('#' + currentTraining + '_tomorrow').DataTable( {
-        data: dataSet,
-        columns: [
-            { title: "ID" },
-            { title: "Nombre" },
-            { title: "Dni" },
-            { title: "Clase" },
-            { title: "Horario" },
-            { title: "Fecha" }        ]
+    $('#' + currentTraining + '_tomorrow').DataTable({
+      data: dataSet,
+      columns: [
+        { title: "ID" },
+        { title: "Nombre" },
+        { title: "Dni" },
+        { title: "Clase" },
+        { title: "Horario" },
+        { title: "Fecha" }]
     });
   });
 }
@@ -105,31 +106,31 @@ const getTurns = (token) => {
 const is_before = (date1, date2) => {
   const arr1 = date1.split("/");
   const arr2 = date2.split("/");
-  if(parseInt(arr1[1]) < parseInt(arr2[1])) return true;
-  if(parseInt(arr1[1]) > parseInt(arr2[1])) return false;
-  if(parseInt(arr1[0]) < parseInt(arr2[0])) return true;
+  if (parseInt(arr1[1]) < parseInt(arr2[1])) return true;
+  if (parseInt(arr1[1]) > parseInt(arr2[1])) return false;
+  if (parseInt(arr1[0]) < parseInt(arr2[0])) return true;
   return false;
 }
 
 const clear_db = (token) => {
   var authSufix = "auth=" + token;
-  $.getJSON('https://il-tempo-dda8e.firebaseio.com/turnos.json?' + authSufix, function(data) {
-    if(data == null) return;
+  $.getJSON('https://il-tempo-dda8e.firebaseio.com/turnos.json?' + authSufix, function (data) {
+    if (data == null) return;
     var dataSet = [];
     const today = new Date();
     const parsed_today = today.getDate() + "/" + (today.getMonth() + 1);
     dataArray = Object.values(data);
     idArray = Object.keys(data);
     count = 0;
-    for(var i = 0; i < dataArray.length; i++) {
+    for (var i = 0; i < dataArray.length; i++) {
       var fecha = dataArray[i].fecha;
-      if(is_before(fecha, parsed_today)) {
+      if (is_before(fecha, parsed_today)) {
         count++;
         $.ajax({
-          url: 'https://il-tempo-dda8e.firebaseio.com/turnos/'+ idArray[i] +'.json?'+ authSufix,
+          url: 'https://il-tempo-dda8e.firebaseio.com/turnos/' + idArray[i] + '.json?' + authSufix,
           type: "DELETE",
-          success: function(data){
-            if(i == dataArray.length - 1) {
+          success: function (data) {
+            if (i == dataArray.length - 1) {
               alert("Se limpiaron" + count + " turnos viejos");
             }
           }
@@ -140,37 +141,37 @@ const clear_db = (token) => {
 }
 
 
-$(document).ready(function() {
+$(document).ready(function () {
   var token = getCookie("token");
-  if(token == "") {
+  if (token == "") {
     var loginUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDMsEID7PGSNpM5EySROO3iA-eUhcO_KPo";
     var response = $.post(loginUrl, {
-              "email": "test@iltempo.com",
-              "password": "contraseña123",
-              "returnSecureToken": true,
-            },function(data, status, jqXHR) {
-              setCookie("token", data["idToken"], data["expiresIn"]);
-              getTurns(data["idToken"]);
-            });
+      "email": "test@iltempo.com",
+      "password": "contraseña123",
+      "returnSecureToken": true,
+    }, function (data, status, jqXHR) {
+      setCookie("token", data["idToken"], data["expiresIn"]);
+      getTurns(data["idToken"]);
+    });
   } else {
     getTurns(token);
   }
 
 });
 
-$('#clear_button').click(function() {
+$('#clear_button').click(function () {
   var token = getCookie("token");
-  if(token == "") {
+  if (token == "") {
     var loginUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDMsEID7PGSNpM5EySROO3iA-eUhcO_KPo";
     var response = $.post(loginUrl, {
-              "email": "test@iltempo.com",
-              "password": "contraseña123",
-              "returnSecureToken": true,
-            },function(data, status, jqXHR) {
-              token = data["idToken"];
-              setCookie("token", token, data["expiresIn"]);
-              clear_db(token);
-            });
+      "email": "test@iltempo.com",
+      "password": "contraseña123",
+      "returnSecureToken": true,
+    }, function (data, status, jqXHR) {
+      token = data["idToken"];
+      setCookie("token", token, data["expiresIn"]);
+      clear_db(token);
+    });
   } else {
     clear_db(token);
   }
@@ -180,39 +181,40 @@ const addTurn = (token) => {
   var authSufix = "auth=" + token;
   var currentTraining = getCurrentTraining();
   var capTraining = currentTraining[0].toUpperCase() + currentTraining.slice(1);
-  if(capTraining == "Funcional") capTraining = "Funcional Grupal";
-  if(capTraining == "Musculacion") capTraining = "Musculación";
+  if (capTraining == "Funcional") capTraining = "Funcional Grupal";
+  if (capTraining == "Musculacion") capTraining = "Musculación";
+  if (capTraining == "Zumba") capTraining = "Ritmos";
   var name = $('#add_' + currentTraining + '_nombre').val();
   var dni = $('#add_' + currentTraining + '_dni').val();
   var fecha = $('#add_' + currentTraining + '_fecha').val();
   var horario = $('#add_' + currentTraining + '_horario').val();
   $.ajax({
-    url: 'https://il-tempo-dda8e.firebaseio.com/turnos.json?'+ authSufix,
+    url: 'https://il-tempo-dda8e.firebaseio.com/turnos.json?' + authSufix,
     type: "POST",
-    data: '{"clase":"' + capTraining + '", "dni": "'+ dni + '", "fecha": "' + fecha +'", "hora": "' + horario + '", "nombre": "' + name + '"}',
-    success: function(data) {
+    data: '{"clase":"' + capTraining + '", "dni": "' + dni + '", "fecha": "' + fecha + '", "hora": "' + horario + '", "nombre": "' + name + '"}',
+    success: function (data) {
       alert("El turno ha sido creado correctamente");
     },
-    error: function(data) {
+    error: function (data) {
       alert("Lo sentimos, hubo un error al crear el turno");
     }
   });
 
 }
 
-$('#add_button').click(function() {
+$('#add_button').click(function () {
   var token = getCookie("token");
-  if(token == "") {
+  if (token == "") {
     var loginUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDMsEID7PGSNpM5EySROO3iA-eUhcO_KPo";
     var response = $.post(loginUrl, {
-              "email": "test@iltempo.com",
-              "password": "contraseña123",
-              "returnSecureToken": true,
-            },function(data, status, jqXHR) {
-              token = data["idToken"];
-              setCookie("token", token, data["expiresIn"]);
-              addTurn(token);
-            });
+      "email": "test@iltempo.com",
+      "password": "contraseña123",
+      "returnSecureToken": true,
+    }, function (data, status, jqXHR) {
+      token = data["idToken"];
+      setCookie("token", token, data["expiresIn"]);
+      addTurn(token);
+    });
   } else {
     addTurn(token);
   }
